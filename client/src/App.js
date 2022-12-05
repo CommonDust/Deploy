c5eee3bee7392245cb604e23e23b424fcf88a2a1
ulises <ulisessahlin@hotmail.com>
2022-12-05 17:00:02 -0300

    Deploy commit

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import {Route} from 'react-router-dom'
+import Home from './components/Home/Home';
+import Details from './components/Details/Details';
+import Form from './components/Form/Form';
+import LandingPage from './components/LandingPage/LandingPage';
+import './App.css';
+import axios from 'axios';
+axios.defaults.baseURL = 'http://localhost:3001/'
+
+function App() {
+  return (
+    <div className="App">
+      <Route exact path="/">
+            <LandingPage/>
+          </Route>
+      <Route exact path='/home' ><Home/></Route>
+      <Route exact path='/dogs/:id'><Details/></Route>
+      <Route exact path='/dog/create'><Form/></Route>
+      
+      
+    </div>
+  );
+}
+
+export default App;
